64692c1ba1a018123dacb96a8ff268bccb25d6d4
milanzzz <milandamoowali@gmail.com>
2024-11-19 16:22:06 +0800

    komit pertama

---
+++ lib/presentation/order/models/order_model.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter_kedai_app/core/extensions/int_ext.dart';
+
+class OrderModel {
+  final String image;
+  final String name;
+  final int price;
+  int quantity;
+
+  OrderModel({
+    required this.image,
+    required this.name,
+    required this.price,
+    this.quantity = 1,
+  });
+
+  String get priceFormat => price.currencyFormatRp;
+}
